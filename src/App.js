f7bcb44e44290b8e629a0721473a53179060ebb8
Rajan <rajafaslur@gmail.com>
2020-08-16 20:10:49 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+  Route
+} from 'react-router-dom'
+import { PlyrComponent } from 'plyr-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+
+
+class App extends React.Component {
+   
+
+ state = {
+  videoID:'SRgwUpyI7t4',
+ }
+  constructor() {
+    super();
+    this.state = {
+      videoID : 'SRgwUpyI7t4',
+      sources: {
+       type: 'video',
+        sources: [
+        {
+            src: this.state.videoID,
+            provider: 'youtube',
+        },
+    ],
+}
+    };
+  }
+  Account ({ match }) {
+    // this.setState({
+    //   videoID: match.params.account
+    // });
+    console.log('hello');
+    //videoID = match.params.account;
+    return <div>
+      <PlyrComponent sources={{
+       type: 'video',
+        sources: [
+        {
+            src: match.params.account,
+            provider: 'youtube',
+        },
+    ],
+      }}/>
     </div>
-  );
+  }
+  render() {
+    
+    console.log('hi')
+    return (
+       
+      <Router>
+       
+        <React.Fragment>
+        <div>
+      
+      </div>
+          <Route path='/:account'  component={this.Account} />
+      
+        </React.Fragment>
+      </Router>
+
+     
+      
+    )
+  }
 }
 
-export default App;
+export default App
