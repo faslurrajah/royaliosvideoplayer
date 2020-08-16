5eab0e7a08755f924e63684fd88cf2ae706b05f2
Rajan <rajafaslur@gmail.com>
2020-08-16 20:19:55 +0530

    Updates

---
+++ precache-manifest.74de870950b687f0daeeddda6a9c3755.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73c38d4c055d68b09225d4308f4dcfe",
+    "url": "/royaliosvideoplayer/index.html"
+  },
+  {
+    "revision": "17866e7036cf7297ad45",
+    "url": "/royaliosvideoplayer/static/css/2.537602a3.chunk.css"
+  },
+  {
+    "revision": "049a29ba2790172a1a1e",
+    "url": "/royaliosvideoplayer/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "17866e7036cf7297ad45",
+    "url": "/royaliosvideoplayer/static/js/2.37946a76.chunk.js"
+  },
+  {
+    "revision": "c42515bd0bbbdae0408e4345b691c51f",
+    "url": "/royaliosvideoplayer/static/js/2.37946a76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "049a29ba2790172a1a1e",
+    "url": "/royaliosvideoplayer/static/js/main.b7b3b4af.chunk.js"
+  },
+  {
+    "revision": "536d785ebc653c825421",
+    "url": "/royaliosvideoplayer/static/js/runtime-main.ed16f23f.js"
+  }
+]);
